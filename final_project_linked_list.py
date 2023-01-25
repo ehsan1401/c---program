b1b49da5d48a0a6b228ab3ba6e11251e980e873b
R3288 <turbo.emamhasani@gmail.com>
2023-01-25 18:17:22 +0330

    1)correct link next -> first 2)add append_second method for second link

--- final_project_linked_list.py
+++ final_project_linked_list.py
@@ -8,36 +8,43 @@ class linked_list:
     def __init__(self):
         self.head = node()
 
-    def append(self,data):
+    def append_first(self,data):
         new_node = node(data)
         cur = self.head
-        while cur.next != None:
-            cur = cur.next
-        cur.next = new_node
+        while cur.first != None:
+            cur = cur.first
+        cur.first = new_node
+    
+    def append_second(self,data):
+        new_node = node(data)
+        cur = self.head
+        while cur.second != None:
+            cur = cur.second
+        cur.second = new_node
 
     def length(self):
         cur = self.head
         total = 0
-        while cur.next != None:
+        while cur.first != None:
             total += 1
-            cur = cur.next
+            cur = cur.first
         return total
         
     def display(self):
         elems = []
         cur_node = self.head
-        while cur_node.next != None:
-            cur_node = cur_node.next
+        while cur_node.first != None:
+            cur_node = cur_node.first
             elems.append(cur_node.data)
         print(elems)
 
     def erase(self, key):
         cur_node = self.head
-        while cur_node.next != None:
+        while cur_node.first != None:
             last_node = cur_node
-            cur_node = cur_node.next
+            cur_node = cur_node.first
             if cur_node.data == key:
-                last_node.next = cur_node.next
+                last_node.first = cur_node.first
                 return
 
 file_list= []
@@ -49,15 +56,17 @@ for i in file_list:
     i = i.replace(",", " ")
     i = i.replace(".", " ")
     file_list2.append(i)
-file_list3 = []
-for i in file_list2:
-    z=i.split()
-    file_list3.append(z)
 
-counter = 0
-for i in file_list3:
-    counter+=1
-    for x in i:
-        z = x.upper()
-        if len(z) > 3:
-            print(z , counter)
+
+# file_list3 = []
+# for i in file_list2:
+#     z=i.split()
+#     file_list3.append(z)
+
+# counter = 0
+# for i in file_list3:
+#     counter+=1
+#     for x in i:
+#         z = x.upper()
+#         if len(z) > 3:
+#             print(z , counter)
